dc6478f461f93361829e57669056ac39e063d35c
Matias Torrico Morgan <matias.dtm@gmail.com>
2019-03-16 10:53:29 -0400

    Primer Commit

---
+++ ProductoBackend/ProductoBackend/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace ProductoBackend.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ProductoBackend.Models.Producto> Productoes { get; set; }
+    }
+}
---
+++ ProductoBackend/ProductoBackend/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductoBackend.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int ProductoID { get; set; }
+        [Required]
+        [Display(Name="Nombre del Producto")]
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
